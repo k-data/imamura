1a063e3f914ae74503ba41a3e693ededb61a0f95
k-data <83756659+k-data@users.noreply.github.com>
2022-04-22 17:02:24 +0900

    newdata

--- streamlit/python/main_streamlit.py
+++ streamlit/python/main_streamlit.py
@@ -257,7 +257,98 @@ elif choice == "ログイン":
 
 
 				elif menu == select[1]:
-					st.write('not data')
+					df_stock = pd.read_csv('streamlit/data/sankisire.csv')
+					df_stock = df_stock.iloc[:-2, :,]
+					df_stock['day'] = df_stock['日付'].apply(lambda x: int(x[-2:]))
+					df_stock['year'] = df_stock['日付'].apply(lambda x: int(x[:4]))
+					df_stock['month'] = df_stock['日付'].apply(lambda x : int(x[5:7]))
+					year_list = [i for i in df_stock['year'].unique()]
+					st.write(year_list)
+					
+					years = st.slider('年を選択してください', min_value=2020, max_value=2022, step=1, value=2020)
+					
+					if years == year_list[0]:
+						df_year = df_stock[df_stock['year'] == 2021]
+						df_month = df_year.groupby('month').sum()
+						df_stock_month = df_month[['金額']]
+						st.bar_chart(df_stock_month)
+						st.write('↓月ごとの仕入れ金額')
+						st.write(df_stock_month.T)
+					elif years == year_list[1]:
+						df_year = df_stock[df_stock['year'] == 2022]
+						df_month = df_year.groupby('month').sum()
+						df_stock_month = df_month[['金額']]
+						st.bar_chart(df_stock_month)
+						st.write('↓月ごとの仕入れ金額')
+						st.write(df_stock_month.T)
+					else:
+						df_year = df_stock[df_stock['year'] == 2020]
+						df_month = df_year.groupby('month').sum()
+						df_stock_month = df_month[['金額']]
+						st.bar_chart(df_stock_month)
+						st.write('↓月ごとの仕入れ金額')
+						st.write(df_stock_month.T)
+
+					con = st.checkbox('all')
+					if con == True:
+
+						df_one = df_stock[df_stock['year'] == 2020]
+						df_month = df_one.groupby('month').sum()
+						df_stock_1 = df_month[['金額']]
+						df_stock_1 = df_stock_1.rename(columns={'金額': 2020})
+						df_stock_1['月'] = df_stock_1.index
+
+
+						df_two = df_stock[df_stock['year'] == 2021]
+						df_month = df_two.groupby('month').sum()
+						df_stock_2 = df_month[['金額']]
+						df_stock_2 = df_stock_2.rename(columns={'金額':2021})
+						df_stock_2['月'] = df_stock_2.index
+
+						df_three = df_stock[df_stock['year'] == 2022]
+						df_month = df_three.groupby('month').sum()
+						df_stock_3 = df_month[['金額']]
+						df_stock_3 = df_stock_3.rename(columns={'金額':2022})
+						df_stock_3['月'] = df_stock_3.index
+
+						df_all = df_stock_1.merge(df_stock_2, how='outer', on='月')
+						df_all = df_all.merge(df_stock_3, how='outer', on='月')
+						df_all.index = df_all['月'].values
+						df_all2 = df_all[[2020, 2021, 2022]]
+						st.line_chart(df_all2)
+						df_year_sum = df_stock.groupby('year').sum()
+						with st.container():
+							col1, col2 = st.columns([1, 1])
+						
+						with col1:
+							st.write(f'年別の{menu}比較')
+							fig, ax = plt.subplots(figsize=(6, 4))
+							x = df_year_sum.index
+							y = df_year_sum['金額'].values
+							sns.barplot(x, y)
+							st.pyplot(fig)
+							st.write('各年の合計金額')
+							st.write(df_year_sum.loc[:,'金額'])
+
+						with col2:
+							st.write(f'月別の{menu}比較')
+
+							x = df_stock_1['月'].values
+							height = df_stock_1[2020].values
+							
+							x2 = df_stock_2['月'].values
+							height2 = df_stock_2[2021].values
+
+							x3 = df_stock_3['月'].values
+							height3 = df_stock_3[2022].values
+
+							fig, axis = plt.subplots(figsize=(8, 6))
+							axis.bar(x, height, label='2020', align='edge')
+							axis.bar(x2, height2, label='2021', align='center')
+							axis.bar(x3, height3, label='2022', align='edge')
+							plt.xticks(np.arange(1, 13, 1))
+							plt.legend()
+							st.pyplot(fig)
 					
 				else:
 					df = pd.read_csv('streamlit/data/sankikougyou.csv')
